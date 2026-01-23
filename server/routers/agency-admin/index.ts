d0d5efcf9b9bd6e86c40c3d608c0abbe8d93d22a
Tanner <tannero20@gmail.com>
2026-01-23 11:06:28 -0800

    chore: Update index.ts
    Auto-committed by Claude Code after file modification

---
+++ server/routers/agency-admin/index.ts
@@ -0,0 +1,41 @@
+/**
+ * Agency Admin Router
+ * Composes all agency management sub-routers
+ *
+ * Sub-modules:
+ * - agency: Agency CRUD operations
+ * - staff: Member management
+ * - protocols: Protocol upload and workflow
+ * - versions: Version control
+ */
+
+import { router } from "../../_core/trpc";
+import { agencyProcedures } from "./agency";
+import { staffProcedures } from "./staff";
+import { protocolProcedures } from "./protocols";
+import { versionProcedures } from "./versions";
+
+export const agencyAdminRouter = router({
+  // Agency management
+  myAgencies: agencyProcedures.myAgencies,
+  getAgency: agencyProcedures.getAgency,
+  updateAgency: agencyProcedures.updateAgency,
+
+  // Staff management
+  listMembers: staffProcedures.listMembers,
+  inviteMember: staffProcedures.inviteMember,
+  updateMemberRole: staffProcedures.updateMemberRole,
+  removeMember: staffProcedures.removeMember,
+
+  // Protocol management
+  listProtocols: protocolProcedures.listProtocols,
+  uploadProtocol: protocolProcedures.uploadProtocol,
+  getUploadStatus: protocolProcedures.getUploadStatus,
+  updateProtocolStatus: protocolProcedures.updateProtocolStatus,
+  publishProtocol: protocolProcedures.publishProtocol,
+  archiveProtocol: protocolProcedures.archiveProtocol,
+
+  // Version control
+  listVersions: versionProcedures.listVersions,
+  createVersion: versionProcedures.createVersion,
+});
